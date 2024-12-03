1ed6d13a66d12cf0ebfda560911cbc4edfbce746
Sasha Berkowsky <snberkowsky@gmail.com>
2024-12-03 16:55:43 +0000

    start day 03

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 mod aoc;
 
 fn main() {
-    const AOC_DAY: u8 = 2;
+    const AOC_DAY: u8 = 3;
 
     match AOC_DAY {
         1 => aoc::day_01::solve(),
         2 => aoc::day_02::solve(),
+        3 => aoc::day_03::solve(),
         _ => println!("Wrong day"),
     }
 }
