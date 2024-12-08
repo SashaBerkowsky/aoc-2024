19e88d83b6e4d62e055c305925ff5ef26f30931f
Sasha Berkowsky <snberkowsky@gmail.com>
2024-12-08 23:51:59 +0000

    day 06 readme

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod aoc;
 
 fn main() {
-    const AOC_DAY: u8 = 5;
+    const AOC_DAY: u8 = 6;
 
     println!("Advent of code day {}:", {AOC_DAY});
     match AOC_DAY {
@@ -10,6 +10,7 @@ fn main() {
         3 => aoc::day_03::solve(),
         4 => aoc::day_04::solve(),
         5 => aoc::day_05::solve(),
+        6 => aoc::day_06::solve(),
         _ => println!("Wrong day"),
     }
 }
